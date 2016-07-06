6c5af82d35de2ef4321db9e37d23380ada5b8d46
MilindPathiyal <milindpathiyal@Milinds-MacBook-Air.local>
2016-07-06 11:22:24 -0700

    resolve conflicts in storyboad

--- TimeTable/ViewController.swift
+++ TimeTable/ViewController.swift
@@ -10,14 +10,29 @@ import UIKit
 
 class CollectionViewController: UICollectionViewController {
 
+    var Array = [String]()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        Array = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday", "[INSERT JOKE]"]
         // Do any additional setup after loading the view, typically from a nib.
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+    }
+    override func collectionView(collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 8
+    }
+    
+    override func collectionView(collectionView: UICollectionView, cellForItemAtIndexPath indexPath: NSIndexPath) -> UICollectionViewCell {
+        var cell = collectionView.dequeueReusableCellWithReuseIdentifier("cell", forIndexPath: indexPath) as UICollectionViewCell
+        
+        
+       var Button = cell.viewWithTag(1) as! UILabel
+       Button.text = Array[indexPath.row]
+        return cell
     }
 
 
