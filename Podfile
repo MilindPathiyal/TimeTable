2efedd5525d22a7fa679b22aae6f331c66e9966c
MilindPathiyal <milindpathiyal@Milinds-MacBook-Air.local>
2016-07-06 17:03:32 -0700

    Updated copy

--- Podfile
+++ Podfile
@@ -7,6 +7,7 @@ target 'TimeTable' do
 
   # Pods for TimeTable
   pod 'M13Checkbox'
+  pod 'IQKeyboardManager'
 
   target 'TimeTableTests' do
     inherit! :search_paths
--- TimeTable/AppDelegate.swift
+++ TimeTable/AppDelegate.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import IQKeyboardManager
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -15,6 +16,8 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
 
     func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
+        //IQKeyboardManager.sharedManager().enable = true
+        //IQKeyboardManager.sharedManager().shouldResignOnTouchOutside = true
         // Override point for customization after application launch.
         return true
     }
